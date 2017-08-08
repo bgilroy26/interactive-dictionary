d8eacfdb72c23971a5f05bd0a77c29476552e960
bgilroy26 <bdgilroy@gmail.com>
2017-08-08 18:07:10 -0400

    Added check for non-existent words; check for words close in spelling using difflib; added quit function

--- word_def.py
+++ word_def.py
@@ -1,8 +1,25 @@
 import json
+import difflib
+import sys
 
 with open('data.json','r') as fh:
     dictionary = json.load(fh)
 
-print(dictionary['rain'])
+def lookup(w):
+    try: 
+        return dictionary[w.lower()]
+    except KeyError:
+        close = [entry for entry in dictionary.keys() if difflib.SequenceMatcher(None, entry, w).ratio() > .75]
+        if close:
+            return "Do you mean any of these words: " + ", ".join(close) + "?"
+        else:
+            return "Word not found"
 
+while True:
+    word = input("Enter word: ")
+
+    if word == 'EXIT':
+        sys.exit()
+
+    print(lookup(word))
 
